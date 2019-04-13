e6c05ca3c5b15b7c96da8109bfb8a553ee57ea6e
Titus de Maupeou <titus@esign.eu>
2019-04-13 13:47:55 +0200

    First commit

---
+++ Assets/CubeJump.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+
+public class CubeJump : MonoBehaviour
+{
+    // Start is called before the first frame update
+    KeyboardInput keyboardInput;
+    int note;
+
+    void Start()
+    {
+        keyboardInput = new KeyboardInput();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //DEC values of midi notes
+        for(int i = 0; i < 83; i++)
+        {
+            float note = MidiMaster.GetKey(i);
+            //check velocity
+            if (note > 0 && note <= 1.0)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime);
+            }
+        }  
+    }
+}
---
+++ Assets/KeyboardInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+
+public class KeyboardInput : MonoBehaviour
+{
+    public int noteNumber;
+    //MidiChannel channel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Started 2");
+        Debug.Log(MidiMaster.GetKnobNumbers(MidiChannel.Ch1));
+        Debug.Log(MidiMaster.GetKnobNumbers(MidiChannel.Ch2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(MidiMaster.GetKeyDown(83));
+    }
+}
